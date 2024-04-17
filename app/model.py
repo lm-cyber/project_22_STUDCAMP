2d308458106caf24ed24c2f2bc9aa41535f40e9d
Nasibullin Alan Maratovich <73221496+lm-cyber@users.noreply.github.com>
2024-04-17 22:12:02 +0300

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,13 +1,36 @@
-from schema import ModelDayInput
-import joblib
+from collections import defaultdict
+import random
 
 
 class Model:
+    """заплатка что с моделью"""
+
     def __init__(self):
-        self.model = joblib.load("./model_ser/model.joblib")
+        self.data_cashe = defaultdict(dict)
+        # k v r
 
     def forward(self, data: ModelDayInput):
-        return self.model(data)
+        moex_df = {
+            "CAPITALIZATION": data.CAPITALIZATION,
+            "CLOSE": data.CLOSE,
+            "DIVISOR": data.DIVISOR,
+            "HIGH": data.HIGH,
+            "LOW": data.LOW,
+            "OPEN": data.OPEN,
+            "TRADEDATE": str(data.TRADEDATE),
+            "finance": data.finance,
+            "economic": data.economic,
+            "politic": data.politic,
+        }
+        val = data.CLOSE
+        for k, v in moex_df.items():
+            if v in self.data_cashe[k]:
+                val += self.data_cashe[k][v]
+            else:
+                r = (random.random() - 0.3) * 6
+                self.data_cashe[k][v] = r
+                val += r
+        return val
 
     def __call__(self, data: ModelDayInput):
         return self.forward(data)
